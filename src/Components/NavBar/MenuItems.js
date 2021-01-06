0ad673ad18477bb1f69a7e6b213cf8534c591ba5
CelinaViscarra <xelaju_celina@yahoo.com>
2021-01-05 20:01:46 -0600

    NaveBar

---
+++ src/Components/NavBar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Employees',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Medical Disabilities',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Users',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Conctact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
