679904c182d2e50125642d38fb76fbf5d960a19b
CelinaViscarra <xelaju_celina@yahoo.com>
2021-01-05 22:30:40 -0600

    NavBar terminada

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -3,14 +3,20 @@ import './Navbar.scss'
 import {MenuItems} from "./MenuItems";
 
 class Navbar extends Component{
+    state = { clicked: false}
+    
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
     render() {
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
-                <div className="menu-icon">
-                    
+                <h1 className="navbar-logo"> FOCUS <i className="fab fa-react"> </i> </h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times': 'fas fa-bars'}> </i>
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index) => {
                         return(
                             <li key={index}> 
@@ -22,6 +28,7 @@ class Navbar extends Component{
                     })}
                     
                 </ul>
+                <button>Sign Up</button>
 
             </nav>
         )
